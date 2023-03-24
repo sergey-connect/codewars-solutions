4b34286de52c2c7a5e0bc3e4cbeb5b4b4c1d436b
sergei-connect <fresh787@yandex.ru>
2023-03-24 14:19:58 +0800

    arrayDeepCount

---
+++ 6-kyu/arrayDeepCount.js
@@ -0,0 +1,30 @@
+/*
+You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+
+Examples
+[]                   -->  0
+[1, 2, 3]            -->  3
+["x", "y", ["z"]]    -->  4
+[1, 2, [3, 4, [5]]]  -->  7
+The input will always be an array.
+*/
+
+function arrayDeepCount(arr) {
+    let length = arr.length;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            length += arr[i].length;
+
+            for (let j = 0; j < arr[i].length; j++) {
+                if (Array.isArray(arr[i][j])) {
+                    length += arr[i][j].length;
+                }
+            }
+        }
+    }
+
+    return length
+}
+
+arrayDeepCount([1, 2, [3, 4, [5]]])
